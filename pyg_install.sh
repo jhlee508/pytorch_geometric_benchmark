e480e846b0fde5020ee7868601f426ec31e3c74d
Jaehwan Lee <chrisjae508@gmail.com>
2025-04-08 11:53:14 +0900

    JH Installation Guide

---
+++ pyg_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+start_time=$(date +%s)
+echo "Starting installation at $(date)"
+
+
+# PyTorch 2.5.0 on CUDA 12.4
+pip3 install torch==2.5.0 torchvision==0.20.0 torchaudio==2.5.0 --index-url https://download.pytorch.org/whl/cu124
+
+# Install PyG (torch_geometric) and additional libraries
+pip3 install torch_geometric==2.5.0
+pip3 install pyg-lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.5.0+cu124.html
+
+pip3 install xxhash
+
+echo "PyTorch and PyG installed successfully."
+echo "Time taken (minutes and seconds): $(( ( $(date +%s) - $start_time ) / 60 ))m $(( ( $(date +%s) - $start_time ) % 60 ))s"
